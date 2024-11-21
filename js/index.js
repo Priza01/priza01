60314be7cf4033bbe762cfccfb2ec9a3dadebc90
bernardinoleite <bernardinoleite2008@gmail.com>
2024-11-21 22:02:11 +0100

    renomeando o _.html para k.html

--- js/index.js
+++ js/index.js
@@ -14,7 +14,7 @@ form.addEventListener("submit", async (e) => {
 
         if (resultado.ok) {
             localStorage.setItem("token", resultado.token);
-            location.href = 'https://priza01.github.io/priza01/_.html'
+            location.href = 'https://priza01.github.io/priza01/k.html'
         }
 
         else {
